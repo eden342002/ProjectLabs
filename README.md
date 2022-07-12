0ed1d6242bca26ca021b94f60999ed685a94784b
eden342002 <eden342002@gmail.com>
2022-07-11 22:32:00 -0500

    First Commit

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
-# ProjectLabs
-Labs for Handson experience with creating AWS Cloud infrastructure using Terraform
+# 23-CC-Task1
+Have to create/launch Application using Terraform
+
+1. Create the key and security group which allow the port 80.
+
+2. Launch EC2 instance.
+
+3. In this Ec2 instance use the key and security group which we have created in step 1.
+
+4. Launch one Volume (EBS) and mount that volume into /var/www/html
+
+5. Developer have uploded the code into github repo also the repo has some images.
+
+6. Copy the github repo code into /var/www/html
+
+7. Create S3 bucket, and copy/deploy the images from github repo into the s3 bucket and change the permission to public readable.
+
+8 Create a Cloudfront using s3 bucket(which contains images) and use the Cloudfront URL to update in code in /var/www/html
+
+"About task1.tf"
+1.
+To go inside directory where " .tf" file is stored.
+ {like:- 
+  #cd desktop
+  #cd hybrid cloud
+  #cd tera
+  #cd 23_task1 }
+2.
+ open that .tf file with notepad
+  #notepad task1.tf
+  (file task1.tf is also attched in the repo)
+3.
+ Run these commands to initialize plugin as well as to run code
+  # terraform init 
+  # terraform validate
+  #terraform apply -auto-approve
+
+*Output is being attaches after ruuning this file:-"output.png
---
+++ task1.tf
@@ -0,0 +1,100 @@
+provider "aws" {
+  region = "ap-south-1"
+  profile = "23-terraform"
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-0447a12f28fddb066"
+  instance_type = "t2.micro"
+  key_name = "23-firstkey"
+  security_groups = [ "23-Task1" ]
+
+  connection {
+    type     = "ssh"
+    user     = "ec2-user"
+    private_key = file("C:/Users/Pratiksha Jain/Downloads/23-firstkey.pem")
+    host     = aws_instance.web.public_ip
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo yum install httpd  php git -y",
+      "sudo systemctl restart httpd",
+      "sudo systemctl enable httpd",
+    ]
+  }
+
+  tags = {
+    Name = "lwos1"
+  }
+
+}
+
+
+resource "aws_ebs_volume" "esb1" {
+  availability_zone = aws_instance.web.availability_zone
+  size              = 1
+  tags = {
+    Name = "lwebs"
+  }
+}
+
+
+resource "aws_volume_attachment" "ebs_att" {
+  device_name = "/dev/sdh"
+  volume_id   = "${aws_ebs_volume.esb1.id}"
+  instance_id = "${aws_instance.web.id}"
+  force_detach = true
+}
+
+
+output "myos_ip" {
+  value = aws_instance.web.public_ip
+}
+
+
+resource "null_resource" "nulllocal2"  {
+	provisioner "local-exec" {
+	    command = "echo  ${aws_instance.web.public_ip} > publicip.txt"
+  	}
+}
+
+
+
+resource "null_resource" "nullremote3"  {
+
+depends_on = [
+    aws_volume_attachment.ebs_att,
+  ]
+
+
+  connection {
+    type     = "ssh"
+    user     = "ec2-user"
+    private_key = file("C:/Users/Pratiksha Jain/Downloads/23-firstkey.pem")
+    host     = aws_instance.web.public_ip
+  }
+
+provisioner "remote-exec" {
+    inline = [
+      "sudo mkfs.ext4  /dev/xvdh",
+      "sudo mount  /dev/xvdh  /var/www/html",
+      "sudo rm -rf /var/www/html/*",
+      "sudo git clone https://github.com/Pratiksha-2305/23-CC-Task1.git /var/www/html/"
+    ]
+  }
+}
+
+resource "null_resource" "nulllocal1"  {
+
+
+depends_on = [
+    null_resource.nullremote3,
+  ]
+
+	provisioner "local-exec" {
+	    command = "start chrome  ${aws_instance.web.public_ip}"
+  	}
+}
+
+
